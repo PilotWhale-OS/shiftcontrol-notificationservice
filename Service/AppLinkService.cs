da036ec4297dc1caed9dddc3c96bdc591729183f
tobeh <66030584+toobeeh@users.noreply.github.com>
2026-01-27 01:29:15 +0100

    #262 Minor notification adaptions, url fixes

--- Service/AppLinkService.cs
+++ Service/AppLinkService.cs
@@ -3,47 +3,60 @@ using NotificationService.Settings;
 
 namespace NotificationService.Service;
 
+public enum LinkMode
+{
+    Relative,
+    Absolute
+}
+
 public class AppLinkService(ILogger<AppLinkService> logger, IOptions<FrontendSettings> frontendSettings)
 {
     private string BaseUrl => frontendSettings.Value.BaseUrl.TrimEnd('/');
 
-    public string BuildEventPageUrl(string eventId)
+    public string BuildEventPageUrl(string eventId, LinkMode mode = LinkMode.Relative)
     {
-        return $"{BaseUrl}/events/{eventId}";
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/events/{eventId}";
     }
 
-    public string BuildEventsPageUrl()
+    public string BuildEventsPageUrl(LinkMode mode = LinkMode.Relative)
     {
-        return $"{BaseUrl}/events";
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/events";
     }
 
-    public string BuildVolunteerDashboardPageUrl(string? eventId)
-    {
-        ArgumentNullException.ThrowIfNull(eventId);
-        return $"{BaseUrl}/events/{eventId}";
-    }
-
-    public string BuildVolunteerPlansPageUrl(string volunteerId)
-    {
-        return $"{BaseUrl}/volunteers/{volunteerId}";
-    }
-
-    public string BuildTrustAlertPageUrl()
-    {
-        return $"{BaseUrl}/trust";
-    }
-
-    public string BuildPlanAssignmentRequestsPageUrl(string? eventId, string? planId)
+    public string BuildVolunteerDashboardPageUrl(string? eventId, LinkMode mode = LinkMode.Relative)
     {
         ArgumentNullException.ThrowIfNull(eventId);
-        ArgumentNullException.ThrowIfNull(planId);
-        return $"{BaseUrl}/events/{eventId}/plans/?planId={planId}&mode=assignments&status=REQUEST_FOR_ASSIGNMENT";
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/events/{eventId}";
     }
 
-    public string BuildPlanVolunteersPageUrl(string? eventId, string? planId)
+    public string BuildVolunteerPlansPageUrl(string volunteerId, LinkMode mode = LinkMode.Relative)
+    {
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/volunteers/{volunteerId}";
+    }
+
+    public string BuildTrustAlertPageUrl(LinkMode mode = LinkMode.Absolute)
+    {
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/trust";
+    }
+
+    public string BuildPlanAssignmentRequestsPageUrl(string? eventId, string? planId, LinkMode mode = LinkMode.Relative)
     {
         ArgumentNullException.ThrowIfNull(eventId);
         ArgumentNullException.ThrowIfNull(planId);
-        return $"{BaseUrl}/events/{eventId}/plans/?planId={planId}&mode=users";
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/events/{eventId}/plans?planId={planId}&mode=assignments&status=REQUEST_FOR_ASSIGNMENT";
+    }
+
+    public string BuildPlanUnassignmentRequestsPageUrl(string? eventId, string? planId, LinkMode mode = LinkMode.Relative)
+    {
+        ArgumentNullException.ThrowIfNull(eventId);
+        ArgumentNullException.ThrowIfNull(planId);
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/events/{eventId}/plans?planId={planId}&mode=assignments&status=AUCTION_REQUEST_FOR_UNASSIGN";
+    }
+
+    public string BuildPlanVolunteersPageUrl(string? eventId, string? planId, LinkMode mode = LinkMode.Relative)
+    {
+        ArgumentNullException.ThrowIfNull(eventId);
+        ArgumentNullException.ThrowIfNull(planId);
+        return $"{(mode == LinkMode.Absolute ? BaseUrl : "")}/events/{eventId}/plans?planId={planId}&mode=users";
     }
 }
